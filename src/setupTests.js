5c070587b66f7d8439e7deca36d11ddbf791f402
Jarvis Black <usnblack@gmail.com>
2020-02-22 12:29:04 -0500

    tests setup but not working

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+
+describe("App", () => {
+  it("mounts to a simulated DOM", () => {
+    const simulatedDOM = render(<App />);
+    const p = simulatedDOM.queryByText(/error/i);
+    expect(p).toBeInTheDocument();
+    console.log(simulatedDOM.debug());
+  });
+});
